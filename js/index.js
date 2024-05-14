a8da837ffc31a712846573ad277673f49080b96b
Adam Maltsagov <adam.maltsagov@gokstadakademiet.no>
2024-05-14 22:18:44 +0200

    oppdatert html, fetchet api, sjekket om jeg får data fra api

---
+++ js/index.js
@@ -0,0 +1,25 @@
+// Globale varibler
+const gameContainer = document.getElementById("games_container"); 
+
+
+
+
+// Api
+const apiKey = "e14d894ca94643c489dcbb1a5f660d26";
+const url = `https://api.rawg.io/api/games?key=${apiKey}`;
+
+// Async await funksjon for fecthe api med get/ Henter spill data
+async function gameData(){
+    try{
+        const res = await fetch(url); 
+        if (!res.ok){
+            throw new Error("Network status not ok")
+        }
+        const gameData = await res.json();
+        console.log(gameData.results);
+        return gameData.results;
+    } catch (err){
+        console.error("Something went wron when getting games", err)
+    }
+}
+gameData()
