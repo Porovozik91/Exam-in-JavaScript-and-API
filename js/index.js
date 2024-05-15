45c414a54cc136aa0ee58338cafbf921165730b4
Adam Maltsagov <adam.maltsagov@gokstadakademiet.no>
2024-05-15 08:09:30 +0200

    gameElements funksjon  som lager elementer.
    updateDisplayGames funksjon som oppdater
    
    lagt til endringer og byttet navn på api fetching

--- js/index.js
+++ js/index.js
@@ -1,5 +1,5 @@
 // Globale varibler
-const gameContainer = document.getElementById("games_container"); 
+const gamesContainer = document.getElementById("games_container");
 
 
 
@@ -9,22 +9,64 @@ const apiKey = "e14d894ca94643c489dcbb1a5f660d26";
 const url = `https://api.rawg.io/api/games?key=${apiKey}`;
 
 // Async await funksjon for fecthe api med get/ Henter spill data
-async function allGamesData(){
-    try{
-        const res = await fetch(url); 
-        if (!res.ok){
+async function fetchAndDisplay() {
+    try {
+        const res = await fetch(url);
+        if (!res.ok) {
             throw new Error("Network status not ok")
         }
         const gameData = await res.json();
         console.log(gameData.results);
-        return gameData.results;
-    } catch (err){
+        updateDisplayGames(gameData.results);
+    } catch (err) {
         console.error("Something went wron when getting games", err)
     }
 }
-allGamesData()
+
+
 
 // Funksjon: lager lokale variabler. Elementer for spill info
-function gameElements(game, gameContainer){
+function gameElements(game, gamesContainer) {
+    //div element for spill informasjon
+    const createGameDiv = document.createElement("div");
+    createGameDiv.classList.add("game_elements");
+    gamesContainer.appendChild(createGameDiv);
+
+    // Spill navn
+    const createGameName = document.createElement("h2");
+    createGameName.classList.add("game_name");
+    createGameName.textContent = game.name;
+    createGameDiv.appendChild(createGameName);
+  
+
+    // Spill bilde
+    const createImage = document.createElement("img");
+    createImage.classList.add("game_image");
+    createImage.src = game.background_image;
+    createImage.alt = game.name;
+    createGameDiv.appendChild(createImage);
+   
+
+    // Spill vurdering
+    const createRating = document.createElement('p');
+    createRating.classList.add("game_rating")
+    createRating.textContent = `Rating: ${game.rating} out of ${game.rating_top}`;
+    createGameDiv.appendChild(createRating);
     
+
+    // styling
+    //bilde
+    createImage.style.width = "300px";
+    createImage.style.height = "170px";
+
+    return createGameDiv;
 }
+
+// funksjon for å itere gjennom spillene og kalle gameElements for hvertspill
+function updateDisplayGames(games) {
+    games.forEach(game => {
+        gameElements(game, gamesContainer);
+    });
+}
+
+fetchAndDisplay()
