2b40394dbfec44ed34eebfadaf0b3811776ff883
Adam Maltsagov <adam.maltsagov@gokstadakademiet.no>
2024-05-15 05:41:11 +0200

    små endringer

--- js/index.js
+++ js/index.js
@@ -9,7 +9,7 @@ const apiKey = "e14d894ca94643c489dcbb1a5f660d26";
 const url = `https://api.rawg.io/api/games?key=${apiKey}`;
 
 // Async await funksjon for fecthe api med get/ Henter spill data
-async function gameData(){
+async function allGamesData(){
     try{
         const res = await fetch(url); 
         if (!res.ok){
@@ -22,4 +22,9 @@ async function gameData(){
         console.error("Something went wron when getting games", err)
     }
 }
-gameData()
+allGamesData()
+
+// Funksjon: lager lokale variabler. Elementer for spill info
+function gameElements(game, gameContainer){
+
+}
