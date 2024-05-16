2041ace29fba8796da7db23323c4e3a4416c1f44
Adam Maltsagov <adam.maltsagov@gokstadakademiet.no>
2024-05-16 12:55:43 +0200

    lagt til game-info.js ordnet navigering til sider

---
+++ js/game-info.js
@@ -0,0 +1,9 @@
+// *Globale variabler
+// Lyttere for to knapper å navigere til sider
+document.getElementById("chooseAnotherBtn").onclick = function() {
+    location.href = "./index.html";
+};
+document.getElementById("myCollectionBtn").onclick = function() {
+    window.location.href = "./my-collection.html";
+};
+
