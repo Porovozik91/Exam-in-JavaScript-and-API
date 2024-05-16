2c71d2c836dd91f28968df4b15e4447b8eee0fff
Adam Maltsagov <adam.maltsagov@gokstadakademiet.no>
2024-05-16 14:46:45 +0200

    laget funksjon for spill elementer

--- js/game-info.js
+++ js/game-info.js
@@ -7,7 +7,7 @@ document.getElementById("myCollectionBtn").onclick = function() {
     window.location.href = "./my-collection.html";
 };
 
-const container = document.getElementById("game_details_container");
+const gameContainer = document.getElementById("game_details_container");
 // Henter game id i parameterverdien fra søkestrengen
 const urlParams = new URLSearchParams(location.search);
 const gameId = urlParams.get("gameId");
@@ -23,10 +23,67 @@ async function displayGameDetails(gameId) {
             throw new Error("Network response was not ok");
         }
         const data = await res.json();
-        console.log(data);
+        //console.log(data);
+        GameDetails(data);
     } catch (error) {
         console.error("Error fetching game details:", error);
     }
+}
+
+function GameDetails(game) {
+
+    // Navn
+    const createGameName = document.createElement("h2");
+    createGameName.classList.add("game_name");
+    createGameName.textContent = game.name;
+    gameContainer.appendChild(createGameName);
+
+    // Bilde
+    const createImage = document.createElement("img");
+    createImage.classList.add("game_image")
+    createImage.src = game.background_image;
+    createImage.alt = game.name;
+    gameContainer.appendChild(createImage);
+
+    const createDescription = document.createElement("p");
+    createDescription.classList.add("game_describe")
+    createDescription.innerHTML = game.description;
+    gameContainer.appendChild(createDescription);
+
+    const createGenres = document.createElement("p");
+    createGenres.classList.add("game_genres");
+    createGenres.textContent = `Genres: ${game.genres.map(genre => genre.name).join(", ")}`;
+    gameContainer.appendChild(createGenres);
+
+    const createReleased = document.createElement("p");
+    createReleased.classList.add("game_released");
+    createReleased.textContent = `Released: ${game.released}`;
+    gameContainer.appendChild(createReleased);
+
+    const createPlatform = document.createElement("p");
+    createPlatform.classList.add("game_platform");
+    createPlatform.textContent = `Platforms: ${game.platforms.map(platform => platform.platform.name).join(", ")}`;
+    gameContainer.appendChild(createPlatform);
+
+    const createRating = document.createElement("p");
+    createRating.classList.add("game_rating");
+    createRating.textContent = `Rating: ${game.rating} out of ${game.rating_top}`;
+    gameContainer.appendChild(createRating);
+
+    const createMetacritic = document.createElement("p");
+    createMetacritic.classList.add("game_metacritic");
+    createMetacritic.textContent = `Metacritic Score: ${game.metacritic}`;
+    gameContainer.appendChild(createMetacritic);
+
+    const createStores = document.createElement("p");
+    createStores.classList.add("game_stores");
+    createStores.textContent = `Where to buy: ${game.stores.map(store => store.store.name).join(", ")}`;
+    gameContainer.appendChild(createStores);
+
+    const createTags = document.createElement("p");
+    createTags.classList.add("game_tags");
+    createTags.textContent = `Tags: ${game.tags.map(tag => tag.name).join(", ")}`;
+    container.appendChild(createTags);
 }
 
 displayGameDetails(gameId);
